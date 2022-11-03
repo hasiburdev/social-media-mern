39934a026a7cb4322bca2c3f8da99e7570e9a258
Hasibur Rahman <hasiburdev@gmail.com>
2022-11-04 01:40:13 +0600

    Update Header text

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
-<h1 className='text-5xl text-red-700' >Hello Kakku</h1>
+      <h1 className="text-5xl text-red-700">Hello</h1>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
